7b7f020021bd9517cfb25038eb4591eb01ce98c6
HFelixH <felix-hoang@outlook.com>
2022-12-01 15:01:22 +1030

    Created Counter app

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useSelector, useDispatch } from 'react-redux';
+import {actions} from './store/index';
+
+function App() {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch(actions.increment())
+  }
+
+  const decrement = () => {
+    dispatch(actions.decrement())
+  }
+
+  const addBy = () => {
+    dispatch(actions.addBy())
+  }
+
+  const subtractBy = () => {
+    dispatch(actions.subtractBy())
+  }
+
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button> 
+      <button onClick={addBy}>Add by 10</button>
+      <button onClick={subtractBy}>Subtract by 10</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,50 @@
+import {configureStore, createSlice} from '@reduxjs/toolkit';
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: {counter: 0},
+    reducers: {
+        increment(state, action) {
+            state.counter ++;
+        },
+        decrement(state, action) {
+            state.counter --;
+        },
+        addBy(state, action) {
+            state.counter += 10;
+        },
+        subtractBy(state, action) {
+            state.counter -= 10;
+        }
+    }
+});
+
+export const actions = counterSlice.actions;
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
+
+export default store;
+
+// const reducerFn = (state={ counter:10 }, action) => {
+
+// //Synchroneous function
+// // We should not mutate the original state
+
+//     if (action.type==='INCREMENT'){
+//         return {counter: state.counter + 1};
+//     }else if (action.type==='DECREMENT'){
+//         return {counter: state.counter - 1};
+//     }else if (action.type==='ADD'){
+//         return {counter: state.counter + action.payload};
+//     }else if (action.type==='SUBTRACT'){
+//         return {counter: state.counter - action.payload};
+//     }else{
+//         return state; // This is the default state
+//     }
+// }
+
+// const store = createStore(reducerFn);
+
+// export default store;
